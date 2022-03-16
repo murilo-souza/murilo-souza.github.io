02082e55aa6af8769dc88ee927d31b95493eb4f7
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2022-03-16 11:14:09 -0300

    update

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -13,15 +13,27 @@ export const GlobalStyles = createGlobalStyle`
         scroll-behavior: smooth
     }
 
-    @media (max-width:1280) {
+    @media (max-width:1080) {
         html {
-            font-size: 93.75%
+            font-size: 93.75%;
         }  
     }
 
     @media (max-width: 720){
         html {
-            font-size: 87.5%
+            font-size: 87.5%;
+        }
+    }
+
+    body {
+        ::-webkit-scrollbar{
+            width: 2em;
+        }
+        ::-webkit-scrollbar-track{
+            background: #232121
+        }
+        ::-webkit-scrollbar-thumb{
+            background: #1C1A1A
         }
     }
 `
