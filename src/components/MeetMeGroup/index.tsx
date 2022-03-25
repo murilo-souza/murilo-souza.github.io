3602c305a00bdd39e43a2810a366966958b75563
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2022-03-25 07:07:51 -0300

    update

--- src/components/MeetMeGroup/index.tsx
+++ src/components/MeetMeGroup/index.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import { Container } from './styles';
+import {Github} from '../MeetmeContent/Github'
+import {Gmail} from '../MeetmeContent/Gmail'
+import {Intagram} from '../MeetmeContent/Instagram'
+import {Linkedin} from '../MeetmeContent/Linkedin'
+
 
 export function MeetMeGroup(){
     return(
-        <Container></Container>
+        <Container>
+            <section>
+                <Github/>
+                <Linkedin/>
+            </section>
+            <section>
+                <Gmail/>
+                <Intagram/>
+            </section>
+        </Container>
     )
 }
