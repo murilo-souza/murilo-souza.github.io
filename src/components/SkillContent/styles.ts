4add1482e716b22ecf6818f644457bce0681691c
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2022-03-30 12:43:54 -0300

    update

--- src/components/SkillContent/styles.ts
+++ src/components/SkillContent/styles.ts
@@ -7,11 +7,16 @@ export const Container = styled.div`
     margin-bottom: 20px;
     :hover {
         div{
-            height: 105px;
-            width: 15px;
+            height: 87.5px;
+            width: 12.5px;
+            transition: all 0.3s;
+        }
+        a{
+            font-size: 62.5px;
             transition: all 0.3s;
         }
     }
+
     div{
        background-color: #ff0084;
        height: 70px;
@@ -25,10 +30,6 @@ export const Container = styled.div`
         font-size: 50px;
         margin-right: 40px;
         text-decoration: none;
-        :hover{
-            font-size: 65px;
-            transition: all 0.3s;
-        }
           
     }
 `;
