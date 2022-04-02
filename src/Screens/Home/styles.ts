ed7782cbe6ac4e5d04b45861cca0d85b9e451661
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2022-04-02 11:00:47 -0300

    update

--- src/Screens/Home/styles.ts
+++ src/Screens/Home/styles.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
     width: 100%;
     height: 100vh;
-    padding: 175px 120px 0 100px;
+    padding: 175px 12   0px 0 100px;
     display: flex;
     flex-direction: row;
     justify-content: space-between;
