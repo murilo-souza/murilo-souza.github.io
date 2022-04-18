ba64ca88829a64f0fef46ccbc5bddeb7d12c506d
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2022-04-17 21:23:02 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    baseURL: 'https://myportfolio-f6dd7-default-rtdb.firebaseio.com/api'
+    baseURL: 'https://localhost:3000/api'
 })
