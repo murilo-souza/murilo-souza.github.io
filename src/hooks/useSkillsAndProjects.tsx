db21b75eb13d45ff49a331faf5ff3cf9630b977d
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2022-04-19 07:27:25 -0300

    upate

--- src/hooks/useSkillsAndProjects.tsx
+++ src/hooks/useSkillsAndProjects.tsx
@@ -1,2 +1,25 @@
 import React from 'react';
 import { db } from '../services/firebase-config';
+import {createContext, ReactNode, useContext, useEffect, useState} from 'react'
+
+
+interface Skill {
+    title: string,
+    alt: string,
+}
+
+interface Project {
+    title: string,
+    alt: string,
+    link: string,
+}
+
+interface SkillsAndProjectsContextData {
+    skills: (skill: Skill[]) => Promise<void>
+    projects: (project: Project[]) => Promise<void>
+}
+
+export const SkillsAndProjectsContext = createContext<SkillsAndProjectsContextData>(
+    {} as SkillsAndProjectsContextData
+)
+
