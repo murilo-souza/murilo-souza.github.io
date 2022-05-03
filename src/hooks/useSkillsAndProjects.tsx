2757c0a7c8f7435061ac0659d2273286f3120687
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2022-05-03 16:13:00 -0300

    Update useSkillsAndProjects.tsx

--- src/hooks/useSkillsAndProjects.tsx
+++ src/hooks/useSkillsAndProjects.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
-<<<<<<< HEAD
-import { db } from '../services/firebase-config';
-=======
 
 export function useSkillsAndProjects(){
     return(
         <div/>
     );
 }
->>>>>>> 4cafde2 (Update useSkillsAndProjects.tsx)
